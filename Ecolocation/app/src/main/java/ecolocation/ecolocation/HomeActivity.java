99c1599749157592ddfd24ba5b82c2a3812008e7
ChandlerAHayes <ChandlerAHayes@gmail.com>
2017-12-03 14:56:07 -0700

    fixed missing gradle files

--- Ecolocation/app/src/main/java/ecolocation/ecolocation/HomeActivity.java
+++ Ecolocation/app/src/main/java/ecolocation/ecolocation/HomeActivity.java
@@ -15,7 +15,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         //----------- Implementing Widgets -------------
         runButton = (Button) findViewById(R.id.bttn_run);
