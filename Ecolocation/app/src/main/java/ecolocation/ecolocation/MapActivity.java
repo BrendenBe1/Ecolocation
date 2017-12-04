f307a7d0b0dd02434b29e5d6c1ca51a53c6f7649
ChandlerAHayes <ChandlerAHayes@gmail.com>
2017-12-03 23:51:41 -0700

    shows user's current location on map

--- Ecolocation/app/src/main/java/ecolocation/ecolocation/MapActivity.java
+++ Ecolocation/app/src/main/java/ecolocation/ecolocation/MapActivity.java
@@ -1,40 +1,215 @@
 package ecolocation.ecolocation;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = MapActivity.class.getSimpleName();
+    private GoogleMap map;
+    private CameraPosition cameraPosition;
+
+    // The entry points to the Places API.
+    private GeoDataClient geoDataClient;
+    private PlaceDetectionClient placeDetectionClient;
+
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private boolean locationPermissionGranted;
+
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location lastKnownLocation;
+    private String[] likelyPlaceNames;
+    private String[] likelyPlaceAddresses;
+    private String[] likelyPlaceAttributions;
+    private LatLng[] likelyPlaceLatLngs;
+
+    //----- CONSTANTS
+    //for default location
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    // Used for selecting the current place.
+    private static final int M_MAX_ENTRIES = 5;
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-                 /*
-        * Setting up the google maps view w/ a fragment
-        * Retreived from https://developers.google.com/maps/documentation/android-api/map-with-marker
-        *
-        * Getting the SupportMapFragment & request notification when map is ready to use
-        */
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        //The first two are primary entry points to the Google Places API & Location Services
+        geoDataClient = Places.getGeoDataClient(this, null);
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //get a handle to the map fragment
         SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_layout);
+                .findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
     }
 
+    /*
+    * Use the fuseLocationProvider to get the device's last-known location & use that to position
+    *   the map.
+    */
+    public void getDeviceLocation(){
+        //get the best & most recent location of the device
+        try{
+            if(locationPermissionGranted){
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if(task.isSuccessful())
+                        {
+                            //set the map's camera position to current location
+                            lastKnownLocation = task.getResult();
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                                    lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude()
+                            ), DEFAULT_ZOOM));
+                        }
+                        else{
+                            //log messages
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+
+                            //move camera & get UI settings
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation,
+                                    DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+
+                    }
+
+                });
+            }
+        }
+        catch(SecurityException e){
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    /*
+    * Request permissions to access user's location but first checks if the location permission was
+    * already granted, if not then it asks.
+    */
+    private void getLocationPermission(){
+        //checks to see if permission is already granted
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationPermissionGranted = true;
+        }
+        //permission is not already granted, need to ask for it
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission
+                .ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * This callback is triggered when the map is ready to be used.
+     */
     @Override
-    public void onMapReady(GoogleMap googleMap){
-        //add a marker in Sydney, Australia
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney))
-                .setTitle("Marker in Sydney");
-        //move map's camera to the marker
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+
+        //TODO: do other activities, will be implemented later
+
+        //TODO: delete?
+        getLocationPermission();
+
+        //turn on My Location Layer & related control on the map
+        updateLocationUI();
+
+        //Get the current location of the device & set the position of the map
+        getDeviceLocation();
+    }
+
+    /*
+    * Handling the result of the permission request
+    */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults){
+        locationPermissionGranted = false;
+        switch (requestCode){
+            //if request is cancelled, the result arrays are empty
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                //if permission is granted, set locationPermissionGranted to true
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    /*
+    * Set the location controls on the map.
+    *
+    * If the user granted location permissions, then enable My Location Layer & related controls
+    *   on the map.
+    *  Otherwise, disable them & set current location to null;
+    */
+    private void updateLocationUI(){
+        if(map == null){
+            return;
+        }
+
+        try{
+            //see if location permission was granted
+            if(locationPermissionGranted){
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+            }
+            //disable My Location layer & controls
+            else{
+                //disable layer & controls
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+
+                //reset lastKnownLocation & ask for permission
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        }
+        catch(SecurityException e){
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
