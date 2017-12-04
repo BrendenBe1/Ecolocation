768600f54381cc782f7738a9898a1e0f4ba5380c
ChandlerAHayes <ChandlerAHayes@gmail.com>
2017-12-04 02:18:21 -0700

    replaced LocationActivity with MapActivity

--- Ecolocation/app/src/main/java/ecolocation/ecolocation/HomeActivity.java
+++ Ecolocation/app/src/main/java/ecolocation/ecolocation/HomeActivity.java
@@ -27,7 +27,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //This tells it what activity to go to & what activity we're on
-                Intent intent = new Intent(HomeActivity.this, LocationActivity.class);
+                Intent intent = new Intent(HomeActivity.this, MapActivity.class);
 
                 startActivity(intent);
             }
